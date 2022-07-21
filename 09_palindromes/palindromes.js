558c002b9f86563810e0b890d4119e637a957f41
murosaka <murosaka@gmail.com>
2022-07-21 16:26:35 +0300

    Finished exercise #09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-zA-Z]+/g, '');
+    let strPal = '';
+    for (let i = str.length - 1; i >=0; i--) {
+      strPal += str[i];
+    }
+    if (str === strPal) {
+      return true
+    }
+    else {  
+      return false;
+    }
 };
 
 // Do not edit below this line
