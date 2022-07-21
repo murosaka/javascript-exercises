57c4c98309d3c16a08bec79c32910e0249f5aed7
murosaka <murosaka@gmail.com>
2022-07-21 15:46:05 +0300

    Finished exercise #08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let summary = 0;
+	for (let i = 0; i <= arr.length - 1; i++) 
+  {
+    summary += arr[i];
+  }
+  return summary;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let summary = 1;
+	for (let i = 0; i <= arr.length - 1; i++) 
+  {
+    summary *= arr[i];
+  }
+  return summary;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fac = 1;
+  if (a == 0) 
+  {
+    fac = 1;
+  }
+  else 
+  {
+    for (let i = a; i >= 1; i--){
+      fac *= i;
+    }
+  }
+	return fac;
 };
 
 // Do not edit below this line
