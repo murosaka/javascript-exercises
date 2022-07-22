33d92a27609b17c2445e5dc7157a433ace91f571
murosaka <murosaka@gmail.com>
2022-07-22 09:21:21 +0300

    111

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let arr = [];
+    let i = 2;
+    arr[0] = 0;
+    arr[1] = 1;
+    // for (i; i <= a; i++) {
+    //     arr[i] = arr[i-1] + arr[i-2];
+    // }
+    return typeof arr;
 };
 
 // Do not edit below this line
