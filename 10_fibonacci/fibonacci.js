112fcaef244e3da601add9aaa8323d27689af7f2
murosaka <murosaka@gmail.com>
2022-07-22 09:37:26 +0300

    Finished exercise #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,10 +3,17 @@ const fibonacci = function(a) {
     let i = 2;
     arr[0] = 0;
     arr[1] = 1;
-    // for (i; i <= a; i++) {
-    //     arr[i] = arr[i-1] + arr[i-2];
-    // }
-    return typeof arr;
+    if (a < 0) 
+    {
+        return 'OOPS'
+    }
+    else 
+    {
+    for (i; i <= a; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[a]
+    }
 };
 
 // Do not edit below this line
