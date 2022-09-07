4e61252f41d6612c706beaa26500c2011c383a6f
murosaka <murosaka@gmail.com>
2022-09-07 09:26:44 +0300

    111111

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+
+const findTheOldest = function(arr) {
+  const result = arr.reduce((prevVal, curVal) =>
+   (prevVal.yearOfDeath-prevVal.yearOfBirth < 
+    curVal.yearOfDeath-curVal.yearOfBirth) ? curVal: prevVal);
+  
+  return result.name;
 };
 
+console.log(findTheOldest(people));
 // Do not edit below this line
-module.exports = findTheOldest;
+// module.exports = findTheOldest;
